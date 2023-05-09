098d546f281c65f9791c275fd649ab568457fa46
Fatima Hassan <46327090+FatimaHassan1@users.noreply.github.com>
2023-05-09 15:50:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+const hijriConverter = require('hijri-converter');
+
+// Convert Gregorian date to Hijri date
+const hijriDate = hijriConverter.convertToHijri(new Date('2023-05-09'));
+console.log(hijriDate); // Output: { year: 1444, month: 2, day: 3 }
+
+// Convert Hijri date to Gregorian date
+const gregorianDate = hijriConverter.convertToGregorian(1444, 2, 3);
+console.log(gregorianDate); // Output: 2023-05-09T00:00:00.000Z
